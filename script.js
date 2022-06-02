2cc00a8022b3746d1d2576c740bd83d20531744c
Denis Homoki <homoki.denisth@gmail.com>
2022-06-02 22:41:28 +0300

    added js

---
+++ script.js
@@ -0,0 +1,19 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const container = document.querySelector(".container");
+const btn = document.querySelector(".main-button");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexValue = "#";
+  for (let i = 0; i < 6; i++) {
+    hexValue += hex[randomNumber()];
+  }
+  color.textContent = hexValue;
+  document.body.style.backgroundColor = hexValue;
+  color.style.color = hexValue;
+});
+
+function randomNumber() {
+  return Math.trunc(Math.random() * hex.length);
+}
